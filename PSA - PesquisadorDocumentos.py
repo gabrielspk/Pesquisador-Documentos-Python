b2154559d7b3fb20b2509c1d177b1867cda4dac0
gabrielspk <gabrielsilvaferreira007@gmail.com>
2024-11-12 10:56:27 -0300

    Adiciona suporte para pesquisa reversa em dados de tabulação

--- PSA - PesquisadorDocumentos.py
+++ PSA - PesquisadorDocumentos.py
@@ -375,7 +375,9 @@ def btn_validar():
     #arquivosValidados.to_excel(planilhaPosValidacao, index=False)
     
 def busca_reversa(diretorio_atual, posicao, comprimento, nomenclaturaDescartada):
-    documentosExtraidos = []
+    documentos_extraidos = []
+    linha_cabecalho = 0
+
     try:
         posicao = int(posicao)
         comprimento = int(comprimento)
@@ -387,11 +389,22 @@ def busca_reversa(diretorio_atual, posicao, comprimento, nomenclaturaDescartada)
 
                     with open(caminho_arquivo, 'r', encoding='utf-8', errors="ignore") as f:
                         for linha in f:
-                            novaPosicao = posicao - 1 if posicao > 1 else 0
-                            documento = linha[novaPosicao:novaPosicao + comprimento]
-                            documentosExtraidos.append(documento)
-        
-        return documentosExtraidos
+                            if file.endswith(".csv") or file.endswith(".CSV"):
+                                    if linha_cabecalho == 0:
+                                        linha_cabecalho += 1
+                                        continue
+                                    tabulador = linha.split(";")
+                                    if posicao >= len(tabulador):
+                                        documentos_extraidos.append("Posição declarada fora do range")
+                                    else:
+                                        nova_posicao = posicao - 1 if posicao > 1 else 0
+                                        documento = tabulador[nova_posicao][:comprimento]
+                                        documentos_extraidos.append(documento)
+                            else:
+                                nova_posicao = posicao - 1 if posicao > 1 else 0
+                                documento = linha[nova_posicao:nova_posicao + comprimento]
+                                documentos_extraidos.append(documento)
+        return documentos_extraidos
     except ValueError:
         return ["Erro: Posição e comprimento precisam ser números inteiros."]
 
